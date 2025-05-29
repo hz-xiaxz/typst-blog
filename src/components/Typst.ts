e5c55f6a2c1e857f3c30d9cc944d19820edd73ca
ParaN3xus <paran3xus007@gmail.com>
2025-05-29 23:08:56 +0800

    chore: remove dead code

--- src/components/Typst.ts
+++ src/components/Typst.ts
@@ -1,5 +1,4 @@
 import { NodeCompiler } from "@myriaddreamin/typst-ts-node-compiler";
-import commentTemplate from "../../typ/templates/comment.typ?raw";
 import { resolve } from "path";
 
 const projectRoot = resolve(
@@ -23,39 +22,4 @@ export async function renderPdf(mainFileContent: string): Promise<Buffer> {
       "build-kind": "monthly",
     },
   });
-}
-
-export async function renderComment(typstCode: string): Promise<string> {
-  let maxRawBackticks = 0;
-  let matchedBackticks;
-  let re = /`{3,}/g;
-  while ((matchedBackticks = re.exec(typstCode)) !== null) {
-    const backtickCount = matchedBackticks[0].length;
-    if (backtickCount > maxRawBackticks) {
-      maxRawBackticks = backtickCount;
-    }
-  }
-
-  const backtick = "`".repeat(Math.max(maxRawBackticks + 1, 3));
-  const mainFileContent = `
-${commentTemplate}
-${backtick}md-render
-${typstCode}
-${backtick}
-`;
-
-  try {
-    compiler.evictCache(10);
-    const result = compiler.tryHtml({ mainFileContent });
-    if (result.hasError()) {
-      console.error("Error compiling comment:");
-      result.printDiagnostics();
-      return "";
-    }
-
-    return result.result?.body() || "";
-  } catch (error) {
-    console.error("Error rendering Typst code:", error);
-    return "";
-  }
 }
